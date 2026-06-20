c031f34f5f904d42c4bf39819ab788db6f86c6b9
Rihan <276017186+bisilByteX@users.noreply.github.com>
2026-06-20 12:18:36 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const form = document.getElementById("patientForm");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const patient = {
+
+        name: document.getElementById("name").value,
+
+        age: document.getElementById("age").value,
+
+        gender: document.getElementById("gender").value,
+
+        dob: document.getElementById("dob").value,
+
+        phone: document.getElementById("phone").value,
+
+        disease: document.getElementById("disease").value,
+
+        address: document.getElementById("address").value
+
+    };
+
+    let patients = JSON.parse(localStorage.getItem("patients")) || [];
+
+    patients.push(patient);
+
+    localStorage.setItem("patients", JSON.stringify(patients));
+
+    alert("✅ Patient Admitted Successfully!");
+
+    form.reset();
+
+});
+
+
+// Future Use
+// Display Patients
+
+function getPatients(){
+
+    return JSON.parse(localStorage.getItem("patients")) || [];
+
+}
+
+
+// Search Patient
+
+function searchPatient(name){
+
+    let patients = getPatients();
+
+    return patients.filter(patient =>
+
+        patient.name.toLowerCase().includes(name.toLowerCase())
+
+    );
+
+}
+
+
+// Delete Patient
+
+function deletePatient(index){
+
+    let patients = getPatients();
+
+    patients.splice(index,1);
+
+    localStorage.setItem("patients",JSON.stringify(patients));
+
+}
+
+
+
+function updatePatient(index,newData){
+
+    let patients = getPatients();
+
+    patients[index]=newData;
+
+    localStorage.setItem("patients",JSON.stringify(patients));
+
+}
